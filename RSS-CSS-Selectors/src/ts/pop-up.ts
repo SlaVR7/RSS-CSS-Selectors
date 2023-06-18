96ba31a027bd5a3b4ef3e3c6aea200c6b0f56021
SlaVR7 <mylife0793@gmail.com>
2023-06-18 14:18:33 +0300

    feat: add popup in adaptive

--- RSS-CSS-Selectors/src/ts/pop-up.ts
+++ RSS-CSS-Selectors/src/ts/pop-up.ts
@@ -1,15 +1,28 @@
 export function movePopup():void {
 
-    const cross: HTMLDivElement | null = document.querySelector('.closeBtn');
-    const menu: HTMLDivElement | null= document.querySelector('.menu');
+    const cross: HTMLDivElement | null = document.querySelector('#closeBtn');
+    const menu: HTMLDivElement | null= document.querySelector('#rightMenu');
     const popup: HTMLDivElement | null = document.querySelector('.pop-up');
+
+    const adaptivePopup: HTMLDivElement | null = document.querySelector('#adaptivePopup');
+    const closeAdaptive: HTMLDivElement | null = document.querySelector('#closeAdaptive');
+    const adaptiveMenu: HTMLDivElement | null = document.querySelector('#adaptiveMenu');
+
 
     function togglePopup(): void {
         if (popup !== null) popup.classList.toggle('hidden');
+    }
+    function toggleAdaptivePopup(): void {
+        if (adaptivePopup !== null) adaptivePopup.classList.toggle('hidden-adaptive');
     }
 
     if (cross !== null && menu !== null) {
         cross.addEventListener('click', togglePopup);
         menu.addEventListener('click', togglePopup);
+    }
+
+    if (closeAdaptive !== null && adaptiveMenu !== null) {
+        closeAdaptive.addEventListener('click', toggleAdaptivePopup);
+        adaptiveMenu.addEventListener('click', toggleAdaptivePopup);
     }
 }
