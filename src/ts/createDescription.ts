6fca749e0447023a39bdca5d259cad6816dea024
SlaVR7 <mylife0793@gmail.com>
2023-09-22 14:42:02 +0300

    Updates

--- src/ts/createDescription.ts
+++
@@ -1,8 +0,0 @@
-import {Levels} from "./level-objects";
-import { Figures } from './figures';
-
-export const taskDescription: NodeListOf<HTMLDivElement> = document.querySelectorAll('.task-description');
-
-export function createDescription(level: Levels<Figures>):void {
-    taskDescription.forEach((item: HTMLDivElement) => item.innerText = level.description);
-}
--- src/ts/fakeLayout.ts
+++
@@ -1,7 +0,0 @@
-import { levelsObjects } from './level-objects';
-
-
-export function createFakeLayout(container: HTMLDivElement | null) {
-    const level: string | null = localStorage.getItem('level');
-    if (container && level) container.innerHTML = levelsObjects[+level - 1].fakeLayout || '';
-}
