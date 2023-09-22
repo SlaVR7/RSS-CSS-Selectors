b89142f3f9cfd627b9ea88afe590831497ac9d08
SlaVR7 <mylife0793@gmail.com>
2023-09-22 14:29:56 +0300

    chore: replace all files

--- RSS-CSS-Selectors/src/ts/buttonListener.ts
+++
@@ -1,6 +0,0 @@
-import { compareResult } from './compareResult';
-import { changeLevelByInput } from './changeLevel';
-export function buttonListener():void {
-    changeLevelByInput(undefined, 'mouse');
-    compareResult(undefined, 'mouse');
-}
